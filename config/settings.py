a8dd58872ae3c586efc26668e3c9352e0bbe726e
Anton Shpakovych <shpakovych18@gmail.com>
2023-11-30 13:51:07 +0200

    Changed channel layers backend to Redis

--- config/settings.py
+++ config/settings.py
@@ -15,8 +15,11 @@ DEBUG = os.getenv("DJANGO_DEBUG_MODE")
 
 CHANNEL_LAYERS = {
     "default": {
-        "BACKEND": "channels.layers.InMemoryChannelLayer"
-    }
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
+    },
 }
 
 CACHES = {
